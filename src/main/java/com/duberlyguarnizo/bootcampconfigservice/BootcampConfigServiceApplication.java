3294e264b54ec2a2dc3bc15be087c493856c610c
Duberly, Guarnizo Fuentes Rivera <duberlygfr@gmail.com>
2023-09-19 18:32:32 -0500

    Initial commit

---
+++ src/main/java/com/duberlyguarnizo/bootcampconfigservice/BootcampConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.duberlyguarnizo.bootcampconfigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampConfigServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootcampConfigServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/duberlyguarnizo/bootcampconfigservice/BootcampConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duberlyguarnizo.bootcampconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
